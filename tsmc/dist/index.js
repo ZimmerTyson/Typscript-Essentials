1873d0e2339663d8c354a7f74729acf14a871dbb
Tyson Zimmer <zimmertyson@gmail.com>
2025-10-15 06:37:23 -0700

    update going over different types and how they react in typescript including arrays and functions

--- tsmc/dist/index.js
+++ tsmc/dist/index.js
@@ -1,13 +1,63 @@
 "use strict";
+// ---------
+// Arrays
+// ---------
 Object.defineProperty(exports, "__esModule", { value: true });
-let age = 30;
-let firstName = 'mario';
-let isFictional;
-let planet = 'Earth';
-let moons = 1;
-let isLarge = false;
-planet = 'saturn';
-moons = 123;
-let something = null;
-let anotherThing = undefined;
-let test = 'this is a test';
+let names = ['mario', 'luigi', 'peach'];
+let ages = [25, 28, 24];
+names.push('toad');
+ages.push(35);
+// ---------
+// type inference with arrays
+// ---------
+// TS will automatically infer what data type you are using if you don't assign one
+// The same output will occur when you attempt to pass a different value type to the array in this case.
+// You will get an error if the data type is different than the existing array data type inferred
+let fruits = ['apples', 'pears', 'mango'];
+fruits.push('peaches');
+const f = fruits[3];
+console.log(f);
+// you can add different value types but when you try to pull a value from the array, it wont
+// know exactly what type it is pulling out. This is called a union type
+let things = [1, true, 'hello'];
+const t = things[0];
+// ---------
+// Object literals
+// ---------
+let user = {
+    firstName: 'mario',
+    age: 30,
+    id: 1,
+};
+user.firstName = 'peach';
+user.id = 2;
+// ---------
+// type inference with object literals
+// ---------
+let person = {
+    name: 'luigi',
+    score: 35
+};
+person.name = 'bowser';
+person.score = 40;
+const score = person.score;
+console.log(score);
+// ---------
+// functions
+// ---------
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumbers = (a, b) => {
+    return a - b;
+};
+addTwoNumbers(3, 9);
+subtractTwoNumbers(10, 3);
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
+// ---------
+// return type inference
+// ---------
--- tsmc/src/index.ts
+++ tsmc/src/index.ts
@@ -1,18 +1,101 @@
-let age: number = 30;
-
-let firstName: string = 'mario';
-let isFictional: boolean;
-
-let planet = 'Earth';
-let moons = 1;
-let isLarge = false;
-
-planet = 'saturn'
-moons = 123;
+// ---------
+// Arrays
+// ---------
 
 
-let something = null;
-let anotherThing = undefined;
+
+let names: string[] = ['mario', 'luigi', 'peach'];
+let ages: number[] = [25, 28, 24];
 
 
-let test = 'this is a test'
+names.push('toad');
+ages.push(35);
+
+
+// ---------
+// type inference with arrays
+// ---------
+
+// TS will automatically infer what data type you are using if you don't assign one
+// The same output will occur when you attempt to pass a different value type to the array in this case.
+// You will get an error if the data type is different than the existing array data type inferred
+
+let fruits = ['apples', 'pears', 'mango'];
+
+fruits.push('peaches');
+
+const f = fruits[3];
+console.log(f);
+
+
+// you can add different value types but when you try to pull a value from the array, it wont
+// know exactly what type it is pulling out. This is called a union type
+
+let things  = [1, true, 'hello']
+
+const t = things[0];
+
+// ---------
+// Object literals
+// ---------
+
+let user: {firstName: string, age: number, id: number} = {
+  firstName: 'mario',
+  age: 30,
+  id: 1,
+}
+
+
+user.firstName = 'peach'
+user.id = 2
+
+// ---------
+// type inference with object literals
+// ---------
+
+
+let person = {
+  name: 'luigi',
+  score: 35
+}
+
+person.name = 'bowser'
+person.score = 40
+
+
+const score = person.score;
+console.log(score);
+
+
+
+// ---------
+// functions
+// ---------
+
+
+function addTwoNumbers(a:number, b:number): number {
+  return a + b
+}
+
+
+const subtractTwoNumbers = (a: number, b: number): number => {
+  return a - b;
+}
+
+addTwoNumbers(3, 9)
+subtractTwoNumbers(10, 3);
+
+
+function addAllNumbers(items: number[]): void {
+  const total = items.reduce((a, c) => a + c, 0)
+  console.log(total);
+
+
+}
+
+addAllNumbers([5, 7, 9, 11, 3, 2, 1])
+
+
+// ---------
+// return type inference
+// ---------
