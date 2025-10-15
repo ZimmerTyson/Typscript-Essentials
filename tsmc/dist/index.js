7577b7c652232b6cc0a8220af3ed9e8c3f429741
Tyson Zimmer <zimmertyson@gmail.com>
2025-10-15 10:48:32 -0700

    expanded typescript notes to cover type guards, tagged interfaces, union types, tuples, interfaces, and any types

--- tsmc/dist/index.js
+++ tsmc/dist/index.js
@@ -61,3 +61,136 @@ addAllNumbers([5, 7, 9, 11, 3, 2, 1]);
 // ---------
 // return type inference
 // ---------
+// if we dont type the return type for functions, it will infer as to what type you actually want to
+// return for you. Just like any other instance, if you attempt to force the output of a type that isn't
+// congruent with the type of data it is receiving, you will get an error.
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+const result = formatGreeting('mario', 'hello');
+// ---------
+// any type
+// ---------
+// the any type is basically reverting back to regular js since it will bypass a specific type
+let age;
+let title;
+age = 30;
+age = false;
+title = 25;
+title = {
+    hello: 'world'
+};
+// ---------
+// any type in arrays
+// ---------
+// the any type in arrays works the same way, you can use and push any type of value and it will allow it
+// but again, this will basically case you to revert back to regular js
+let newThings = ['hello', true, 30, null];
+// ---------
+// functions & any
+// ---------
+function addTogether(value) {
+    return value + value;
+}
+const resultOne = addTogether('hello');
+// useful when migrating from js to ts
+// because using any won't cause errors initially
+// ---------
+// tuples
+// ---------
+let newPerson = ['mario', 30, true];
+// ---------
+// tuples examples
+// ---------
+let hsla;
+hsla = [200, '100%', '50%', 1];
+let xy;
+xy = [98.2, 20.1];
+function useCoord() {
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+const [lat, long] = useCoord();
+// ---------
+// named tuples
+// ---------
+let newUser;
+newUser = ['peach', 25];
+console.log(newUser);
+const authorOne = { name: 'mario', avatar: '/img/mario/png' };
+const newPost = {
+    title: 'my first post',
+    body: 'something interesting',
+    tags: ['gaming', 'tech'],
+    create_at: new Date(),
+    author: authorOne,
+};
+// ---------
+// as function argument types
+// ---------
+function createPost(post) {
+    console.log(`Created post ${post.title} by ${post.author.name}`);
+}
+createPost(newPost);
+// ---------
+// with arrays
+// ---------
+let posts = [];
+posts.push(newPost);
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+const userOne = { name: 'mario', score: 75 };
+function formatUser(user) {
+    console.log(`${user.name} has a score of ${user.score}`);
+}
+formatUser(userOne);
+formatUser({ name: 'yoshi', score: 100 });
+// ---------
+// union type
+// ---------
+let someId;
+someId = 1;
+someId = '2';
+let email = null;
+email = 'mario@netninja.dev';
+email = null;
+let anotherId;
+anotherId = '124kjdsflkj';
+anotherId = 23423;
+// ---------
+// union type pitfalls
+// ---------
+// function swapIdType(id: Id): Id {
+//   // can only use props and methods common to
+//   // both number and string types
+//   // parseInt(id) --> not allowed
+//   // parseInt(5)
+//   return id
+// }
+// ---------
+// type guards
+// ---------
+function swapIdType(id) {
+    if (typeof id === 'string') {
+        // can use string methods now
+        return parseInt(id);
+    }
+    else {
+        // can use number methods and properties
+        return id.toString();
+    }
+}
+const idOne = swapIdType(1);
+const idTwo = swapIdType('2');
+console.log(idOne, idTwo);
+// ---------
+// tagged interfaces
+// ---------
--- tsmc/src/index.ts
+++ tsmc/src/index.ts
@@ -99,3 +99,258 @@ addAllNumbers([5, 7, 9, 11, 3, 2, 1])
 // ---------
 // return type inference
 // ---------
+
+// if we dont type the return type for functions, it will infer as to what type you actually want to
+// return for you. Just like any other instance, if you attempt to force the output of a type that isn't
+// congruent with the type of data it is receiving, you will get an error.
+
+
+function formatGreeting (name: string, greeting: string) {
+  return  `${greeting}, ${name}`
+}
+
+const result = formatGreeting('mario', 'hello');
+
+
+// ---------
+// any type
+// ---------
+
+// the any type is basically reverting back to regular js since it will bypass a specific type
+
+
+let age: any;
+let title;
+
+age = 30;
+age = false;
+
+title = 25
+title = {
+  hello: 'world'
+}
+
+
+// ---------
+// any type in arrays
+// ---------
+
+// the any type in arrays works the same way, you can use and push any type of value and it will allow it
+// but again, this will basically case you to revert back to regular js
+
+let newThings: any[] = ['hello', true, 30, null]
+
+
+
+// ---------
+// functions & any
+// ---------
+
+function addTogether(value: any): any {
+  return value + value;
+}
+
+const resultOne = addTogether('hello');
+
+
+// useful when migrating from js to ts
+// because using any won't cause errors initially
+
+
+
+// ---------
+// tuples
+// ---------
+
+let newPerson: [string, number, boolean] = ['mario', 30, true]
+
+
+// ---------
+// tuples examples
+// ---------
+
+let hsla: [number, string, string, number]
+
+hsla = [200, '100%', '50%', 1]
+
+
+let xy: [number, number]
+
+xy = [98.2, 20.1]
+
+function useCoord(): [number, number] {
+  const lat = 100;
+  const long = 50;
+
+  return [lat, long]
+}
+
+const [lat, long] = useCoord();
+
+// ---------
+// named tuples
+// ---------
+
+
+let newUser: [name: string, age: number]
+
+newUser = ['peach', 25]
+console.log(newUser)
+
+
+// ---------
+// interfaces
+// ---------
+
+interface Author {
+  name: string,
+  avatar: string,
+}
+
+const authorOne: Author = {name: 'mario', avatar: '/img/mario/png'}
+
+
+interface Post {
+  title: string,
+  body: string,
+  tags: string[],
+  create_at: Date,
+  author: Author
+}
+
+const newPost: Post = {
+  title:'my first post',
+  body: 'something interesting',
+  tags: ['gaming', 'tech'],
+  create_at: new Date(),
+  author: authorOne,
+}
+
+
+// ---------
+// as function argument types
+// ---------
+
+
+function createPost(post: Post):void {
+  console.log(`Created post ${post.title} by ${post.author.name}`)
+}
+
+createPost(newPost)
+
+// ---------
+// with arrays
+// ---------
+
+
+let posts: Post[] = [];
+
+posts.push(newPost)
+
+
+
+// ---------
+// type aliases
+// ---------
+
+//example 1 - tuple
+
+type Rgb = [number, number , number]
+
+function getRandomColor(): Rgb {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return [r, g, b]
+}
+
+const colorOne = getRandomColor()
+const colorTwo = getRandomColor()
+ console.log(colorOne, colorTwo);
+
+//example 2 - object literal
+
+type User = {
+  name: string,
+  score: number
+}
+
+const userOne: User = {name: 'mario', score: 75}
+
+function formatUser(user: User): void {
+  console.log(`${user.name} has a score of ${user.score}`)
+}
+
+formatUser(userOne);
+formatUser({ name: 'yoshi', score: 100 })
+
+
+
+// ---------
+// union type
+// ---------
+
+
+let someId: number | string
+
+
+someId = 1;
+someId = '2';
+
+
+let email: string | null = null
+
+email = 'mario@netninja.dev'
+email = null
+
+type Id = number | string
+
+let anotherId: Id;
+
+anotherId = '124kjdsflkj';
+anotherId = 23423;
+
+
+
+// ---------
+// union type pitfalls
+// ---------
+
+
+// function swapIdType(id: Id): Id {
+
+//   // can only use props and methods common to
+//   // both number and string types
+//   // parseInt(id) --> not allowed
+
+//   // parseInt(5)
+//   return id
+// }
+
+
+
+// ---------
+// type guards
+// ---------
+
+function swapIdType(id: Id) {
+  if (typeof id === 'string') {
+    // can use string methods now
+
+    return parseInt(id)
+  } else {
+    // can use number methods and properties
+
+    return id.toString()
+  }
+}
+
+const idOne = swapIdType(1);
+const idTwo = swapIdType('2');
+
+console.log(idOne, idTwo);
+
+// ---------
+// tagged interfaces
+// ---------
